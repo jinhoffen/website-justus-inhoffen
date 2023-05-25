915fd2accb767cfec26ef97f59f0a43e81e1734f
Justus Inhoffen <jinhoffen@tinbergen.nl>
2023-05-25 16:56:51 +0200

    added toggle option for abstract

--- assets/js/interactions.js
+++ assets/js/interactions.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('a.show-abstract').forEach(el => {
     el.addEventListener('click', (e) => {
         const sender = e.target;
-        const parent = sender.parentNode.parentNode.parentNode.parentNode;
+        const parent = sender.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
         const toggleTarget = parent.querySelector('.abstract-hidden');
         toggleTarget.classList.toggle('abstract-visible');
     });
